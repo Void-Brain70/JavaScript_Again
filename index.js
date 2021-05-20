150170e854c7e009fd92050b8b6b9f5215930731
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2021-05-20 12:09:47 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+console.time("time start");
+console.log("JavaScript");
+
+console.log(70+70);
+
+console.log([1,2,3,4,5]);
+console.log({name:'Anik',id:'70'});
+console.table({name:'Anik',id:'70'});
+
+console.warn('Warning');
+
+console.timeEnd("done")
+
+
+console.assert(age> 200,)
