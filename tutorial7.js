493e4924f629a3e66b4bd4c01148a99e0b6eaa66
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2021-05-21 13:03:29 +0600

    Add files via upload

---
+++ tutorial7.js
@@ -0,0 +1,36 @@
+//operator
+//Arithmetic Operators(+.-,*,/,%,**)(++,--)
+console.log(5+5);
+console.log(5-5);
+console.log(5*5);
+console.log(5/5);
+console.log(6%5);//remainder 
+console.log(5**5);//power
+
+var a=5;
+console.log(++a);//preincrement
+console.log(a++);//postincrement
+
+//Assignment operators(=,+=,-=,*=,/=,%=,**=)
+var a = 6;
+a+=6;//a=a+6
+document.write(a);
+
+var a = 5;
+a*=6;//a=a+6
+document.write(a);
+
+var a = 4;
+a**=2;//a=4**2=16
+document.write(a);
+
+var a = 4;
+a/=2;//a=4/2=2
+document.write(a);
+
+
+
+
+
+
+
