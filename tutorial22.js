423ec99d4a7f2c606af675f391603870f728b1dc
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2021-05-22 16:13:51 +0600

    Add files via upload

---
+++ tutorial22.js
@@ -0,0 +1,28 @@
+//date object
+
+var d=new Date()
+document.write(d);
+
+var y=d.getFullYear();
+document.write(y);
+
+var m= d.getMonth();
+document.write(m);
+
+var cd=d.getDate();
+console.log(cd);
+
+var cd=d.getDate();
+console.log(cd);
+
+var cda=d.getDay();
+console.log(cda);
+
+var ch=d.getHours();
+console.log(ch);
+
+var cm=d.getMinutes0(); 
+console.log(cm);
+
+
+
