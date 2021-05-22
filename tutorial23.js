59b99658aca09b9a985675df0e7476114bd066ae
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2021-05-22 20:31:22 +0600

    Add files via upload

---
+++ tutorial23.js
@@ -0,0 +1,22 @@
+//DOM select HTML elements
+
+//getElementById
+
+// document.getElementById("name").innerHTML="MOMO";
+
+// document.getElementById("ja").innerHTML="Pyth on";
+
+// document.getElementById('u').innerHTML="University of Asia Pacific";
+
+
+//queary selector
+
+document.getElementById("name").innerHTML="Das";
+
+document.querySelector("#ja").innerHTML="Python";
+
+document.querySelector(".t").innerHTML="JavaScript Tutorial";
+
+document.querySelector("p").innerHTML="JavaScript";
+
+
---
+++ tutorial24.js
@@ -0,0 +1,9 @@
+//query selector
+
+document.querySelector("li a").innerHTML="new text";
+
+document.querySelector("div a").innerHTML="Uap";
+
+document.querySelector(".mydiv a").innerHTML="Uap";
+
+document.querySelectorAll("p")[1].innerHTML="Uap";
