5044101c7cc3361d7da53153c81442244baa5494
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2021-05-23 08:46:48 +0600

    Add files via upload

---
+++ tutorial25.js
@@ -0,0 +1,36 @@
+//event handler to on click event
+
+// function msg()
+// {
+//     alert("Hello javascript");
+// }
+
+// function msg1()
+// {
+//     alert("javascript program");
+// }
+
+// function msg()
+// {
+//     var a=document.querySelector("#p");
+//     a.innerHTML="Hello javascript";
+// }
+
+// function msg1()
+// {
+//     var a=document.querySelector("#p");
+//     a.innerHTML="Javascript Code";
+// }
+
+var a=document.querySelector("#img");
+function msg()
+{
+    a.src="image/js.png";
+}
+
+function msg1()
+{
+    a.src="image/python.jpg";
+}
+
+ 
