5b00f8a7e9bd494a6a9102f1fadc8fa5e3c0a5a2
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2021-05-27 10:47:15 +0600

    Add files via upload

---
+++ tutorial36.js
@@ -0,0 +1,22 @@
+//ES6 
+
+let x= 20;
+let y=30;
+
+let s=x+y;
+
+console.log(`sum is ${s} .the end`);
+
+
+let name="Anik";
+let msg ="UAP student";
+
+console.log(`${name} ${msg}`);
+
+//function
+const add=(x,y)=>
+{
+    let s=x+y;
+    console.log(s);
+}
+add(10,10);
